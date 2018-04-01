1f17c6aa28c293f6ce646a16ede37604b0643e04
Mariam Omar <umarsamiya@gmail.com>
2018-04-01 23:35:54 +0300

    add changes

--- user_test.py
+++ user_test.py
@@ -1,7 +1,6 @@
 import unittest
 from user import User
 
-
 class TestUser(unittest.TestCase):
     def setUp(self):
 
@@ -14,6 +13,25 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.phone, "072345678")
         self.assertEqual(self.new_user.email, "mariam@cm")
         self.assertEqual(self.new_user.password, "")
+
+    def test_init_(self):
+
+        '''
+        test case to if the User object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name, "seth")
+        self.assertEqual(self.new_user.password, "password")
+
+    def test_save_user(self):
+
+        '''
+        test case to test if user objects have been saved into user_list
+        '''
+
+        self.new_user.save_user()  # save new user
+        self.assertEqual(len(User.user_list), 1)
+
 
     if __name__ == '__main__':
         unittest.main()
